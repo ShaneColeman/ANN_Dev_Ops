3415588affec96b60927747ea979c326447e49f2
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-03-11 18:56:36 +0000

    ANNDataSetSetupTest class added, ANN saved

---
+++ src/test/java/ANNDataSetSetupTest.java
@@ -0,0 +1,27 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.neuroph.core.data.DataSet;
+
+
+/**
+ * Created by Windows on 11/03/2016.
+ */
+public class ANNDataSetSetupTest {
+
+    @org.junit.Test
+    public void testSetColumnNames() throws Exception
+    {
+        DataSet data = DataSet.createFromFile("C:\\Users\\Windows\\Desktop\\TestingDataSet50.txt", 8, 4, "\t", false);
+
+        ANNDataSetSetup ann = new ANNDataSetSetup();
+
+        ann.setColumnNames(data);
+
+        int expected = 12;
+        int actual = 12;
+
+        Assert.assertEquals(expected,actual);
+    }
+}
